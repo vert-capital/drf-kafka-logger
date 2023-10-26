973ba331b38c36f4b52404ee67ad2240e9868c49
Caio Faria <caio.faria@vert-capital.com>
2023-10-26 11:49:00 -0300

    upgrade perfomace of task

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="drf_kafka_logger",
-    version="1.0.1",
+    version="1.0.3",
     author="VertCapital",
     author_email="caiofaria@vert-capital.com",
     description="Logger to consumer kafka",
